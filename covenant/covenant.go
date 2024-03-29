6ef925886f1b7e466a2f6c49add8668ecb50878e
Fangyu Gai <greferry@gmail.com>
2024-03-29 19:33:20 +0800

    hotfix: StakingOutputIdx is not set in delegation type conversion

--- covenant/covenant.go
+++ covenant/covenant.go
@@ -3,10 +3,11 @@ package covenant
 import (
 	"bytes"
 	"fmt"
-	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 	"strings"
 	"sync"
 	"time"
+
+	"github.com/btcsuite/btcd/btcec/v2/schnorr"
 
 	"github.com/avast/retry-go/v4"
 	"github.com/btcsuite/btcd/btcec/v2"
@@ -169,6 +170,13 @@ func (ce *CovenantEmulator) AddCovenantSignatures(btcDels []*types.Delegation) (
 			uint16(unbondingTime),
 			&ce.config.BTCNetParams,
 		); err != nil {
+			ce.logger.Error("invalid delegation",
+				zap.String("staker_pk", bbntypes.NewBIP340PubKeyFromBTCPK(btcDel.BtcPk).MarshalHex()),
+				zap.String("finality_provider_pk", bbntypes.NewBIP340PubKeyFromBTCPK(btcDel.FpBtcPks[0]).MarshalHex()),
+				zap.String("staking_tx_hex", btcDel.StakingTxHex),
+				zap.String("slashing_tx_hex", btcDel.SlashingTxHex),
+				zap.Error(err),
+			)
 			return nil, fmt.Errorf("invalid txs in the delegation: %w", err)
 		}
 
--- testutil/datagen.go
+++ testutil/datagen.go
@@ -7,13 +7,30 @@ import (
 	"time"
 
 	sdkmath "cosmossdk.io/math"
+	"github.com/babylonchain/babylon/btcstaking"
 	"github.com/babylonchain/babylon/testutil/datagen"
+	bstypes "github.com/babylonchain/babylon/x/btcstaking/types"
 	"github.com/btcsuite/btcd/btcec/v2"
+	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
+	"github.com/btcsuite/btcd/txscript"
+	"github.com/btcsuite/btcd/wire"
 	"github.com/stretchr/testify/require"
 
 	"github.com/babylonchain/covenant-emulator/types"
 )
+
+type TestStakingSlashingInfo struct {
+	StakingTx   *wire.MsgTx
+	SlashingTx  *bstypes.BTCSlashingTx
+	StakingInfo *btcstaking.StakingInfo
+}
+
+type spendableOut struct {
+	prevOut wire.OutPoint
+	amount  btcutil.Amount
+}
 
 func GenRandomHexStr(r *rand.Rand, length uint64) string {
 	randBytes := datagen.GenRandomByteArray(r, length)
@@ -66,4 +83,125 @@ func GenBtcPublicKeys(r *rand.Rand, t *testing.T, num int) []*btcec.PublicKey {
 	}
 
 	return pks
+}
+
+func GenBTCStakingSlashingInfo(
+	r *rand.Rand,
+	t testing.TB,
+	btcNet *chaincfg.Params,
+	stakerSK *btcec.PrivateKey,
+	fpPKs []*btcec.PublicKey,
+	covenantPKs []*btcec.PublicKey,
+	covenantQuorum uint32,
+	stakingTimeBlocks uint16,
+	stakingValue int64,
+	slashingAddress string,
+	slashingRate sdkmath.LegacyDec,
+	slashingChangeLockTime uint16,
+) *TestStakingSlashingInfo {
+	// an arbitrary input
+	unbondingTxFee := r.Int63n(1000) + 1
+	spend := makeSpendableOutWithRandOutPoint(r, btcutil.Amount(stakingValue+unbondingTxFee))
+	outPoint := &spend.prevOut
+	return GenBTCStakingSlashingInfoWithOutPoint(
+		r,
+		t,
+		btcNet,
+		outPoint,
+		stakerSK,
+		fpPKs,
+		covenantPKs,
+		covenantQuorum,
+		stakingTimeBlocks,
+		stakingValue,
+		slashingAddress,
+		slashingRate,
+		slashingChangeLockTime,
+	)
+}
+
+func makeSpendableOutWithRandOutPoint(r *rand.Rand, amount btcutil.Amount) spendableOut {
+	out := randOutPoint(r)
+
+	return spendableOut{
+		prevOut: out,
+		amount:  amount,
+	}
+}
+
+func GenBTCStakingSlashingInfoWithOutPoint(
+	r *rand.Rand,
+	t testing.TB,
+	btcNet *chaincfg.Params,
+	outPoint *wire.OutPoint,
+	stakerSK *btcec.PrivateKey,
+	fpPKs []*btcec.PublicKey,
+	covenantPKs []*btcec.PublicKey,
+	covenantQuorum uint32,
+	stakingTimeBlocks uint16,
+	stakingValue int64,
+	slashingAddress string,
+	slashingRate sdkmath.LegacyDec,
+	slashingChangeLockTime uint16,
+) *TestStakingSlashingInfo {
+
+	stakingInfo, err := btcstaking.BuildStakingInfo(
+		stakerSK.PubKey(),
+		fpPKs,
+		covenantPKs,
+		covenantQuorum,
+		stakingTimeBlocks,
+		btcutil.Amount(stakingValue),
+		btcNet,
+	)
+
+	require.NoError(t, err)
+	tx := wire.NewMsgTx(2)
+
+	// 2 outputs for changes and staking output
+	changeAddrScript, err := datagen.GenRandomPubKeyHashScript(r, btcNet)
+	require.NoError(t, err)
+	require.False(t, txscript.GetScriptClass(changeAddrScript) == txscript.NonStandardTy)
+
+	tx.AddTxOut(wire.NewTxOut(10000, changeAddrScript)) // output for change
+
+	// add the given tx input
+	txIn := wire.NewTxIn(outPoint, nil, nil)
+	tx.AddTxIn(txIn)
+	tx.AddTxOut(stakingInfo.StakingOutput)
+
+	// construct slashing tx
+	slashingAddrBtc, err := btcutil.DecodeAddress(slashingAddress, btcNet)
+	require.NoError(t, err)
+
+	slashingMsgTx, err := btcstaking.BuildSlashingTxFromStakingTxStrict(
+		tx,
+		uint32(1),
+		slashingAddrBtc,
+		stakerSK.PubKey(),
+		slashingChangeLockTime,
+		2000,
+		slashingRate,
+		btcNet)
+	require.NoError(t, err)
+	slashingTx, err := bstypes.NewBTCSlashingTxFromMsgTx(slashingMsgTx)
+	require.NoError(t, err)
+
+	return &TestStakingSlashingInfo{
+		StakingTx:   tx,
+		SlashingTx:  slashingTx,
+		StakingInfo: stakingInfo,
+	}
+}
+
+func randOutPoint(r *rand.Rand) wire.OutPoint {
+	hash, _ := chainhash.NewHash(datagen.GenRandomByteArray(r, chainhash.HashSize))
+	// TODO this will be deterministic without seed but for now it is not that
+	// important
+	idx := r.Uint32()
+
+	return wire.OutPoint{
+		Hash:  *hash,
+		Index: idx,
+	}
 }
